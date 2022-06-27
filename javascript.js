f8c5ed88ed7b941b146c66abcdcb5ac77564a44d
Shiela <sheshemarie31@gmail.com>
2022-06-27 16:17:41 +0800

    hide and appear modal

--- javascript.js
+++ javascript.js
@@ -3,12 +3,12 @@ const addBookBtn = document.querySelector('.add-book-btn');
 const modal = document.querySelector('.modal-container');
 const modalCloseBtn = document.querySelector('.modal-close-btn');
 
-//to pop-up modal
+//to pop-up modal 
 addBookBtn.addEventListener('click', () => {
     modal.classList.add('active');
 })
 
-//to hide modal 
+//to hide modal
 modalCloseBtn.addEventListener('click', () => {
     modal.classList.remove('active');
 })
