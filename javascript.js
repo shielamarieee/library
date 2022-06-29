8aad188bd18568de9a6813c766837a80e227e01f
Shiela <sheshemarie31@gmail.com>
2022-06-29 18:07:09 +0800

    create object constructors

--- javascript.js
+++ javascript.js
@@ -3,6 +3,8 @@ const addBookBtn = document.querySelector('.add-book-btn');
 const modal = document.querySelector('.modal-container');
 const modalCloseBtn = document.querySelector('.modal-close-btn');
 
+const submitBtn = document.querySelector('.button');
+
 //to pop-up modal 
 addBookBtn.addEventListener('click', () => {
     modal.classList.add('active');
@@ -11,4 +13,8 @@ addBookBtn.addEventListener('click', () => {
 //to hide modal
 modalCloseBtn.addEventListener('click', () => {
     modal.classList.remove('active');
+})
+
+submitBtn.addEventListener('click', () => {
+    alert('nice');
 })
